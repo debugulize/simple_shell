1ef9aa5d06ae9b0ae294c3330918aff0cc8d5b75
debugulize <wanjikumary631@gmail.com>
2022-05-06 07:28:42 -0700

    betty style

---
+++ hello.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/* printf() displays the string inside quotation*/
+
+int main(void)
+/* integer main*/
+
+{
+/*printing hello world*/
+
+printf("Hello, World!");
+return (0);
+}
